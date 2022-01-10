2f76eed29e42e2f7d7a0199ee10fba801c3ba4de
Ajibade <ajibadeemmanuel58@gmail.com>
2022-01-10 19:22:24 +0100

    fixing the social media redirect links

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+
 const Contact = () => {
+    
+    
     return ( 
+
         <div className="container text-center m-3">
             <h2>Contact Ajibade</h2>
             <div className="container row">
@@ -30,15 +33,17 @@ const Contact = () => {
                     <h6 className="m-5">You can reach out to me on social media</h6>
                     <ul className="list-inline">
                         
+                        
+                        
+                        {/* <li className="list-inline-item"><a rel={"noreferrer"}  className="fa fa-whatsapp" target="_blank" href={"https://wa.me/2348178555400"}/></li> */}
+                        
 
-                        <li className="list-inline-item"><a rel={'external'} className="fa fa-whatsapp" target="_blank" href={"https://wa.me/2348178555400"}/></li>
-                        <li className="list-inline-item"><a rel={'external'} className="fas fa-envelope-square" target="_blank" href={"https://ajibadeemmanuel58@gmail.com"}/></li>
-                        <li className="list-inline-item"><a rel={'external'} className="fab fa-linkedin-in" target="_blank" href={"https://www.linkedin.com/in/ajibade-emmanuel-411916178/"}/></li>
-                        <li className="list-inline-item"><a rel={'external'} className="fab fa-facebook-f" target="_blank" href={"https://www.facebook.com/adetomiwa.emmanuel"}/></li>
-                        <li className="list-inline-item"><a rel={'external'} className="fab fa-twitter" target="_blank" href={"https://twitter.com/Ajy_update"} /></li>
-                        <li className="list-inline-item"><a rel={'external'} className="fab fa-github" target="_blank" href={"https://github.com/Ajyupdate"}/></li>
-                         
-
+                        <li className="list-inline-item"><a target="_blank" rel="noreferrer" href="https://wa.me/2348178555400"><i className="fa fa-whatsapp"></i></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noreferrer" href="https://ajibadeemmanuel58@gmail.com"><i className="fas fa-envelope-square"></i></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/ajibade-emmanuel-411916178/"><i className="fab fa-linkedin-in"></i></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noreferrer" href="https://www.facebook.com/adetomiwa.emmanuel"><i className="fab fa-facebook-f"></i></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noreferrer" href="https://twitter.com/Ajy_update"><i className="fab fa-twitter"></i></a></li>
+                        <li className="list-inline-item"><a target="_blank" rel="noreferrer" href="https://github.com/Ajyupdate"><i className="fab fa-github"></i></a></li> 
                     </ul>
 
                     
