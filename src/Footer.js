742eb473a91c231a07af6d4ac7293f2ebc213e41
Ajibade <ajibadeemmanuel58@gmail.com>
2022-01-31 10:15:57 +0100

    removing fixed footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,10 @@
 const Footer = () => {
     return ( 
-        <div className="Footer fixed-bottom ">
+        <div className="Footer  ">
             <p className=" text-center text-white bg-dark p-2">&copy; Ajibade Emmanuel</p>
         </div>
-     );
+    );
+    //  );Revoluntionary principle, text pawn send blessing adesiji gmail to add you to the community, joy ollie, G_makunde,text or fill adora google form, text azyzag adekunle and be friends with her if you can exchange opportunities,  text iconicshotsNdev if his company needs a FE, message foundational black designer and try to be friends if she can share you some of the frontend intenships
 }
  
 export default Footer;
